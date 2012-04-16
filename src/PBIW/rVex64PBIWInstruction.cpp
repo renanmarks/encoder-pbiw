ae817211b99f6ee7974aefae6372688a352fb5aa
Renan A. Marks <contato@renanmarks.net>
2012-04-15 22:13:49 -0400

    Correção: Reordenação de operandos não corrigiam os indices no padrão em casos pouco comuns.

--- src/PBIW/rVex64PBIWInstruction.cpp
+++ src/PBIW/rVex64PBIWInstruction.cpp
@@ -272,6 +272,7 @@ namespace PBIW
           if (newIndex == 6 && this->opBRFslot.getValue() > -1)
             newIndex++;
           
+          pattern->updateIndexes(operandIt->getIndex(), newIndex);
           operandIt->setIndex(newIndex);
         }
         
@@ -280,7 +281,7 @@ namespace PBIW
         operands.assign(operandsTemp.begin(), operandsTemp.end());
       }
       
-      // The slot at index 5 is occupied
+      // The slot is occupied
       if (operands.size() >= size)
       {
         // If we're lucky...!
