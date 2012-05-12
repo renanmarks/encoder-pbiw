715ec285ec84d6c28fff88becbb2f5f5152a1ad5
Renato <renatocox@gmail.com>
2012-05-11 22:53:08 -0400

    Conjunto mínimo de padrões para um conjunto de programas.

---
+++ src/PBIW/PBIWOptimizerDataSet.h
@@ -0,0 +1,45 @@
+/* 
+ * File:   PBIWOptimizerDataSet.h
+ * Author: renato
+ *
+ * Created on May 11, 2012, 5:20 PM
+ */
+
+#ifndef PBIWOPTIMIZERDATASET_H
+#define	PBIWOPTIMIZERDATASET_H
+
+#include <deque>
+#include <iostream>
+#include "PBIWOptimizerJoinPatterns.h"
+
+namespace PBIW
+{
+    using namespace Interfaces;
+    
+    class PBIWOptimizerDataSet {
+    public:
+        PBIWOptimizerDataSet();
+        PBIWOptimizerDataSet(const PBIWOptimizerDataSet& orig);
+        virtual ~PBIWOptimizerDataSet();
+        
+        typedef std::deque<PBIWOptimizerJoinPatterns> Optimizers;
+        typedef std::deque<IPBIWPattern*> Patterns;
+        typedef std::vector<IPBIWPattern*> VectorPatterns;
+        
+        virtual void minimumPatterns();
+        
+        virtual void printPatterns() const;
+        
+        virtual Optimizers getOptimizers() const
+        {   return Optimizers(optimizers.begin(), optimizers.end());  }
+        
+        virtual void setOptimizers(const PBIWOptimizerJoinPatterns& optimizer);
+        
+    private:
+        Optimizers optimizers;
+        Patterns uniquePatterns;
+    };
+}
+
+#endif	/* PBIWOPTIMIZERDATASET_H */
+
